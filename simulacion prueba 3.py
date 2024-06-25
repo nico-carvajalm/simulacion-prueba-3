36986434a6639147e5539eed75f5b44985c948d6
nico-carvajalm <nico.carvajalm@duocuc.cl>
2024-06-25 11:56:19 -0400

    simulación prueba 3 v1

---
+++ simulacion prueba 3.py
@@ -0,0 +1,11 @@
+def mostrar_menu():
+    print("1. Registrar pedido")
+    print("2. Listar todos los pedidos")
+    print("3. Imprimir hoja de ruta")
+    print("4. Salir del programa")
+
+##MAIN
+
+mostrar_menu()
+x=input("Elegir opción: ")
+
