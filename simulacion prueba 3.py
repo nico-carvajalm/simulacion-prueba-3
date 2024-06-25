28eb228268c4238ac70277349395dbc899f12690
nico-carvajalm <nico.carvajalm@duocuc.cl>
2024-06-25 12:17:25 -0400

    registrar pedido

--- simulacion prueba 3.py
+++ simulacion prueba 3.py
@@ -4,8 +4,32 @@ def mostrar_menu():
     print("3. Imprimir hoja de ruta")
     print("4. Salir del programa")
 
+def registrar_pedido():
+    
+    nom=input("Ingrese nombre y Apellido: ")
+    com=input("Ingrese comuna: ")
+    c5=int(input("Ingrese cantidad de cilindros de 5kg: "))
+    c15=int(input("Ingrese cantidad de cilindros de 15kg: "))
+    c45=int(input("Ingrese cantidad de cilindros de 45kg: "))
+    L1=[nom,com,c5,c15,c45]
+    L.append(L1)
+
+
+    
+
 ##MAIN
 
-mostrar_menu()
-x=input("Elegir opción: ")
+L=[]
+while True:
+    mostrar_menu()
+
+    op=input("Elegir opción: ")
+    if op=="1":
+        registrar_pedido()
+        print(L)
+    elif op=="4":
+        print("Saliendo del programa")
+        break
+
+   
 
